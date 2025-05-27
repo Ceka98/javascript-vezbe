d3da81ef2c74e1a9f60100525adbd4afa05afc09
Ceka98 <stefi.ceka98@gmail.com>
2025-05-27 21:15:08 +0200

    Ispravka 1. zadatka

--- Domaci1.js
+++ Domaci1.js
@@ -1,67 +1,79 @@
 // ZADATAK 1
 
-const niz1 = ["Nikola", "Milan", "Mladen", "Vojin", "Darko", "Milica"];
-const niz2 = niz1.filter(function PrvoSLovoM(imaM) {
-  return imaM.includes("M");
-});
+const niz1 = ["Milan", "Mladen", "Vojin", "Darko", "Milica", "Vladimir"];
+const niz2 = niz1
+  // .map(function VelikoSlovoM(imaM) {
+  //   if (imaM.includes("m")) {
+  //     return imaM.toUpperCase();
+  //   }
+  //   return imaM;
+  // }) ----> prvo sam ovako ali mi se nije svidjalo sto mi Vladimira izlogjue sa velika slova
+  // pa sam se setio da mogu dva puta da pozovem funkciju .includes()  :)
+  .filter(function PrvoSLovoM(imaM) {
+    if (imaM.includes("M")) {
+      return imaM;
+    } else if (imaM.includes("m")) {
+      return imaM;
+    }
+  });
 console.log(niz2);
 
-// ZADATAK 2
+// // ZADATAK 2
 
-const niz3 = [
-  {
-    id: 141596489,
-    ime: "Dragoslav",
-    datumPrijave: "2025-05-26T17:36:22.035",
-  },
-  {
-    id: 04537893554,
-    ime: "Jelena",
-    datumPrijave: "2025-05-26T17:36:22.035",
-  },
-  {
-    id: 6477889897,
-    ime: "Petar",
-    datumPrijave: "2025-05-26T17:36:22.035",
-  },
-];
+// const niz3 = [
+//   {
+//     id: 141596489,
+//     ime: "Dragoslav",
+//     datumPrijave: "2025-05-26T17:36:22.035",
+//   },
+//   {
+//     id: 04537893554,
+//     ime: "Jelena",
+//     datumPrijave: "2025-05-26T17:36:22.035",
+//   },
+//   {
+//     id: 6477889897,
+//     ime: "Petar",
+//     datumPrijave: "2025-05-26T17:36:22.035",
+//   },
+// ];
 
-const niz4 = niz3.map(function IdBrojevi(element) {
-  return element.id;
-});
-console.log(niz4);
+// const niz4 = niz3.map(function IdBrojevi(element) {
+//   return element.id;
+// });
+// console.log(niz4);
 
-// ZADATAK 3
+// // ZADATAK 3
 
-const niz5 = [16, 4.6, 8, -10, 0, 1.5, 67];
-// PRIMER JEDAN
+// const niz5 = [16, 4.6, 8, -10, 0, 1.5, 67];
+// // PRIMER JEDAN
 
-const niz6 = niz5.map(function umanjivanje(x) {
-  return x / 2;
-});
-console.log(niz6);
+// const niz6 = niz5.map(function umanjivanje(x) {
+//   return x / 2;
+// });
+// console.log(niz6);
 
-// PRIMER DVA
+// // PRIMER DVA
 
-const niz7 = niz5.map((x) => x / 2);
-console.log(niz7);
+// const niz7 = niz5.map((x) => x / 2);
+// console.log(niz7);
 
-// ZADATAK 4
+// // ZADATAK 4
 
-const niz8 = [
-  { ime: "Marko", godine: 16 },
-  { ime: "Slavica", godine: 19 },
-  { ime: "Zeljko", godine: 18 },
-  { ime: "Tina", godine: 17 },
-  { ime: "Petar", godine: 18 },
-  { ime: "Milca", godine: 19 },
-];
-const niz9 = niz8
-  .filter(function punoletni(element1) {
-    if (element1.godine > 17) return element1.godine;
-  })
-  .map(function samoImena(element1) {
-    return element1.ime;
-  });
+// const niz8 = [
+//   { ime: "Marko", godine: 16 },
+//   { ime: "Slavica", godine: 19 },
+//   { ime: "Zeljko", godine: 18 },
+//   { ime: "Tina", godine: 17 },
+//   { ime: "Petar", godine: 18 },
+//   { ime: "Milca", godine: 19 },
+// ];
+// const niz9 = niz8
+//   .filter(function punoletni(element1) {
+//     if (element1.godine > 17) return element1.godine;
+//   })
+//   .map(function samoImena(element1) {
+//     return element1.ime;
+//   });
 
-console.log(niz9);
+// console.log(niz9);
