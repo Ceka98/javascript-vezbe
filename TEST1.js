ec1126778f06505fe9e1cd2b8b4d3823632bc524
Ceka98 <stefi.ceka98@gmail.com>
2025-07-03 00:10:02 +0200

    TEST1 FINAL

--- TEST1.js
+++ TEST1.js
@@ -4,35 +4,37 @@ import { zaposleni } from "./zaposleni.js";
 
 // ZADATAK 1
 
-// function formatiraj(proizvod) {
-//   return `${proizvod.title} ($${proizvod.price} x ${proizvod.stock})`;
-// }
+function formatiraj(proizvod) {
+  return `${proizvod.title} ($${proizvod.price} x ${proizvod.stock})`;
+}
 
 // // a)
 
-// let proizvodID = proizvodi.filter((proizvod) => {
-//   return proizvod.id === 49;
-// });
-// let izvestajProizvoda49 = proizvodID.map(formatiraj);
-// console.log(izvestajProizvoda49);
+// let proizvodID = proizvodi
+//   .filter((proizvod) => {
+//     return proizvod.id === 49;
+//   })
+//   .map(formatiraj);
+// console.log(proizvodID);
 
 // b)
 
-// let proizvodiZaTrebovanje = proizvodi.filter((proizvod) => {
-//   return proizvod.stock < 10;
-// });
-// let izvestajTrebovanja = proizvodiZaTrebovanje.map(formatiraj);
-// console.log(izvestajTrebovanja);
+// let proizvodiZaTrebovanje = proizvodi
+//   .filter((proizvod) => {
+//     return proizvod.stock < 10;
+//   })
+//   .map(formatiraj);
+// console.log(proizvodiZaTrebovanje);
 
 // c)
 
-// let skupeNamirnice = proizvodi.filter((proizvod) => {
-//   return proizvod.category === "groceries" && proizvod.price > 10;
-// });
-
-// let izvestaj = skupeNamirnice.map(formatiraj);
+// let skupeNamirnice = proizvodi
+//   .filter((proizvod) => {
+//     return proizvod.category === "groceries" && proizvod.price > 10;
+//   })
+//   .map(formatiraj);
 // console.log(
-//   `${izvestaj} Ukupno ima ${izvestaj.length} namirnica sa cenom preko $10`
+//   `${skupeNamirnice} Ukupno ima ${skupeNamirnice.length} namirnica sa cenom preko $10`
 // );
 
 // ZADATAK 2
@@ -74,10 +76,76 @@ import { zaposleni } from "./zaposleni.js";
 //   return `${osoba.reviewerName} (${osoba.reviewerEmail})`;
 // }
 
-// let losiOcenjivaci = ocene.filter((osoba) => {
-//   return osoba.rating === 1;
-// });
+// let losiOcenjivaci = ocene
+//   .filter((osoba) => {
+//     return osoba.rating === 1;
+//   })
+//   .map(formatirajOcenjivace);
 // console.log(losiOcenjivaci);
 
-// let izvestaj = losiOcenjivaci.map(formatirajOcenjivace);
-// console.log(izvestaj);
+// Zadatak 3
+
+// let spisakProizvoda = [
+//   "APPLE",
+//   150,
+//   "PERFUME",
+//   "BREAD",
+//   -17.99,
+//   "EGG",
+//   "JUICE",
+//   0,
+//   "ICE CREAM",
+// ];
+
+// function nasumicniBroj() {
+//   return Math.floor(Math.random() * 100);
+// }
+// let sredjeniProzivodi = spisakProizvoda.filter((proizvd) => {
+//   if (typeof proizvd === "string") {
+//     return proizvd;
+//   }
+// });
+// let konacniSpisak = sredjeniProzivodi.reduce((objekat, proizvod) => {
+//   let vrednost;
+//   let x = nasumicniBroj();
+//   if (x % 3 === 0) {
+//     vrednost = x;
+//   } else {
+//     vrednost = proizvod.toLowerCase();
+//   }
+//   return { ...objekat, [proizvod]: vrednost };
+// }, {});
+// console.log(konacniSpisak);
+
+//  Zadatak 4
+
+// let noviClanovi = zaposleni
+//   .reduce((noviNiz, radnik) => {
+//     return [
+//       ...noviNiz,
+//       {
+//         id: 31,
+//         firstName: "Allie",
+//         lastName: "Clarks",
+//         gender: "female",
+//         age: 27,
+//         email: "alliescat@gmail.com",
+//         role: "user",
+//       },
+//       radnik,
+//       {
+//         id: 32,
+//         firstName: "Johnathan",
+//         lastName: "Silver",
+//         gender: "male",
+//         age: 31,
+//         email: "silver.johnathon@yahoo.com",
+//         role: "moderator",
+//       },
+//     ];
+//   }, [])
+//   .sort((osobaA, osobaB) => {
+//     if (osobaA.age > osobaB.age) return 1;
+//     if (osobaA.age < osobaB.age) return -1;
+//   });
+// console.log(noviClanovi);
