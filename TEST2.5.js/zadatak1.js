eb769c74bb83085299f5e28af50e54763e8d9955
Ceka98 <stefi.ceka98@gmail.com>
2025-12-04 20:50:51 +0100

    Test2.5.dopuna.js

--- TEST2.5.js/zadatak1.js
+++ TEST2.5.js/zadatak1.js
@@ -4,22 +4,24 @@ class Kutija {
   visina;
 
   constructor(sirina, duzina, visina) {
-    this.sirina = sirina;
-    this.duzina = duzina;
+    this.#sirina = sirina;
+    this.#duzina = duzina;
     this.visina = visina;
   }
 
-  izračunajZapreminu() {}
+  izracunajZapreminu() {
+    return this.#sirina * this.#duzina * this.visina;
+  }
 }
 
 class SpecijalnaKutija extends Kutija {
   constructor(visina) {
-    super(10, 10);
+    super(10, 10, visina);
     this.visina = visina;
   }
 
   prikaz() {
-    return `${this.visina}cm`;
+    return `[${this.visina}cm]`;
   }
 }
 
@@ -45,29 +47,48 @@ class Skladiste {
   }
 
   prikaz() {
-    return this.matrica;
+    let prikazVisina = [];
+    for (let i = 0; i < this.matrica.length; i++) {
+      let noviRed = [];
+      for (let j = 0; j < this.matrica[i].length; j++) {
+        let visinaKutije = `${this.matrica[i][j].visina}cm`;
+        noviRed.push(visinaKutije);
+      }
+      prikazVisina.push(noviRed);
+    }
+    return prikazVisina;
   }
-}
 
-let novoSkladiste = new Skladiste(500);
-novoSkladiste.utovari([
-  [4, 5],
-  [5, 4],
-]);
-console.log(novoSkladiste.prikaz());
+  izracunajTrenutniKapacitet() {
+    let zbir = 0;
+    for (let i = 0; i < this.matrica.length; i++) {
+      for (let j = 0; j < this.matrica[i].length; j++) {
+        zbir += this.matrica[i][j].izracunajZapreminu();
+      }
+    }
+    return zbir;
+  }
 
-// koja sabira zapreminu svih kutija u matrici i vraća ukupan zbir, odnosno trenutno zauzeti kapacitet skladišta.
+  izracunajPreostaliKapacitet() {
+    let kapacitet = this.izracunajTrenutniKapacitet();
+    let razlika = this.maxKapacitet - kapacitet;
+    return razlika;
+  }
 
-function izračunajTrenutniKapacitet(matrica) {
-  let zbir = 0;
-  for (let i = 0; i < matrica.length; i++) {
-    for (let j = 0; j < matrica[i].length; j++) {
-      const kutija = matrica[i][j];
-      zbir = zbir + kutija.sirina * kutija.duzina * kutija.visina;
+  istovari() {
+    if (this.izracunajPreostaliKapacitet() > 0) {
+      return 0;
+    }
+    if (this.izracunajPreostaliKapacitet() < 0) {
     }
   }
-  return zbir;
 }
-console.log(izračunajTrenutniKapacitet(novoSkladiste));
 
-function izbaci() {}
+let novoSkladiste = new Skladiste(2000);
+novoSkladiste.utovari([[4], [5], [5], [4]]);
+console.log(novoSkladiste.prikaz());
+console.log(novoSkladiste.izracunajTrenutniKapacitet());
+console.log(novoSkladiste.matrica);
+console.log(novoSkladiste.izracunajPreostaliKapacitet());
+
+// koja sabira zapreminu svih kutija u matrici i vraća ukupan zbir, odnosno trenutno zauzeti kapacitet skladišta.
